388c2e49e45c3f4a1000a3a07be0c736a78184a2
Tsinat7 <tsnat100@gmail.com>
2025-07-12 17:12:50 +0300

    Add UserProfile component using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <UserProfile name="Tsinat" age="32" bio="Loves hiking and playing football" />
       <Footer />
     </div>
   );
