b41316633c189aedbc96b1db317a7ff143c3216b
Tsinat7 <tsnat100@gmail.com>
2025-07-12 15:51:58 +0300

    Initial React app setup

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to ALX React App</h1>
+      <p>This is my first React project using Vite.</p>
+    </div>
+  );
+}
+
+export default App;
