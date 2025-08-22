1d9a8c9ab3611f2ae0b0cf901817e173c272d99f
Tsinat7 <tsnat100@gmail.com>
2025-08-22 14:45:25 +0300

    Rename Formik form with small letter f

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "src/components/FormikForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
